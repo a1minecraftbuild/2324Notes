47ada7d79ea1705803b4452f140702d5719f6f07
Foundling <a1minecraftytchannel@gmail.com>
2023-10-18 09:17:17 -0500

    added python material folder into the repo

---
+++ PythonMaterials/Notes/CSEUnit3/HelloWorld.py
@@ -0,0 +1,15 @@
+print("Hello World")
+#This is a comment so it doesn't run........msoghuoeajugueqofg93ht9ug4tbiq89hui3yugqgqeihgighiqwbtqutui3qt87wihhe87uhq3tiuw4
+"""
+This is a block comment
+It sort of doesn't run.  
+
+"""
+
+#print("heheheha")  this doesn't run
+print("Hello from python")
+print(2+2)
+#built in f(x) -> print(x)
+#       functionName(parameters)
+print() #produce a blank line
+print(2+3)
---
+++ PythonMaterials/Notes/CSEUnit3/UserInput.py
@@ -0,0 +1,3 @@
+#print("Hey, how was break?") no need for this
+ui = input("Hey, how was break? ") #will obtain input from the terminal
+print(ui)
